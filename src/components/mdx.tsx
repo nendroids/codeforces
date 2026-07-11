d85b0381cde3dc5f66075131792818057bf30aad
Harsh <harsh.github.microsoft@gmail.com>
2026-07-11 07:18:34 +0530

    metadataBase property in metadata export is not set for resolving social open graph or twitter images, using "http://localhost:3000". See https://nextjs.org/docs/app/api-reference/functions/generate-metadata#metadatabase

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -1,9 +1,17 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
 
+import { CodeBlock, Pre } from 'fumadocs-ui/components/codeblock';
+import * as TabsComponents from 'fumadocs-ui/components/tabs';
+import { ImageZoom } from 'fumadocs-ui/components/image-zoom';
+
 export function getMDXComponents(components?: MDXComponents) {
   return {
     ...defaultMdxComponents,
+    img: (props) => <ImageZoom {...(props as any)} />,
+    ...CodeBlock,
+    ...Pre,
+    ...TabsComponents,
     ...components,
   } satisfies MDXComponents;
 }
